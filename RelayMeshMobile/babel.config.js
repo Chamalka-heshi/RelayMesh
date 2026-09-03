98a8ecc40fb257415e6598c9317addfcf02f4a99
osalhk <hkosalgeesararahula@gmail.com>
2026-09-03 09:55:35 +0530

    fix(babel): fix WatermelonDB decorators compilation for SDK 57

--- RelayMeshMobile/babel.config.js
+++ RelayMeshMobile/babel.config.js
@@ -1,11 +1,18 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          // Ensure TypeScript transforms 'declare' before class properties
+          lazyImports: true,
+        },
+      ],
+    ],
     plugins: [
       ['@babel/plugin-proposal-decorators', { legacy: true }],
       ['@babel/plugin-transform-class-properties', { loose: true }],
-      ['@babel/plugin-transform-private-methods', { loose: true }],
     ],
   };
 };
